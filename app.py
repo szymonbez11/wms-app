da87390d45c2b685fdba356eb611df5e101c986f
szymonbez11 <szymonbez11@gmail.com>
2026-04-23 21:48:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -425,5 +425,6 @@ if __name__ == "__main__":
     ensure()
     load_db()
     load_workers()
-  port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
